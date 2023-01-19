60fa97ad11e6e78a5fffe1cfadd0366cc42372a8
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2023-01-19 11:01:54 +0700

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "wow-deep-blue": "#001233",
+        "wow-coral-red": "#FF595A",
+        "wow-beige": "#CAC0B3",
+        "wow-white": "#FFFF",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+};
