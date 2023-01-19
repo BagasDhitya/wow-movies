dcee4225b52a8af627c860f3daf9e12503e90a3a
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2023-01-19 18:02:32 +0700

    routing and delete unused css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Landing from './pages/landing'
+import Register from './pages/register'
+import Login from './pages/login'
+import Home from './pages/home'
+import Favorites from './pages/favorites'
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/favorites' element={<Favorites />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
