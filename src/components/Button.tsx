a1ded30a1e780c9567c0453d9b4f4e5833d04ddf
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2023-01-19 19:09:09 +0700

    restructuring button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,24 +3,26 @@ import React, { ButtonHTMLAttributes, FC } from 'react'
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
     type?: any;
     title?: string;
-    label: string;
     id: string;
 }
 
-const Button: FC<Props> = ({ type, label, id, onClick, title }) => {
+const Button: FC<Props> = ({ type, id, onClick, title }) => {
 
     const long = 'bg-wow-coral-red w-80 h-12 text-white rounded-md'
     const medium = 'bg-wow-coral-red w-60 h-12 text-white rounded-md'
     const short = 'bg-wow-coral-red w-40 h-12 text-white rounded-md'
 
     return (
-        <button className={type === 'long' ?
-            long :
-            `${type === 'medium' ?
-                medium : `${type === 'short' && short
+        <button
+            id={id}
+            onClick={onClick}
+            className={type === 'long' ?
+                long :
+                `${type === 'medium' ?
+                    medium : `${type === 'short' && short
+                    }`
                 }`
-            }`
-        }>
+            }>
             {title}
         </button>
     )
--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,25 +1,32 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
 import Layout from '../../components/Layout'
 import Button from '../../components/Button'
 
 const Landing = () => {
+
+    const navigate = useNavigate()
+
     return (
         <Layout>
-            <div className='p-52'>
-
+            <div className='p-52 bg-wow-coral-red'>
             </div>
             <div className='p-52'>
                 <h1 className='text-wow-coral-red'>Watch Your Taste!</h1>
                 <div className='mt-10 mr-30'>
-                    <p className='text-wow-white'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    <p className='text-wow-white'>Presented for those of you who want to find films based on your imagination and fantasy</p>
                 </div>
                 <div className='mt-10'>
                     <Button
                         id='started'
-                        label='started'
                         title='Get Started'
                         type={'medium'}
+                        onClick={() => navigate('/register')}
                     />
+                </div>
+                <div className='mt-5'>
+                    <button onClick={() => navigate('/login')} className='text-wow-white text-sm font-12'>Already have an account? Click here!</button>
                 </div>
             </div>
         </Layout>
